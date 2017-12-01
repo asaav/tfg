7b3c99da9748974742e258f595df5e2d19599506
salk0 <antonio.s814@gmail.com>
2017-12-01 12:55:51 +0100

    Usando 4 algoritmos de substraccion de fondo

--- main.py
+++ main.py
@@ -3,16 +3,17 @@ import cv2
 import sys
 import time
 
+substractor = None
 
-def scale_frame(img, factor):
+def scale_image(img, factor):
     height, width = img.shape[:2]
     scaledw = int(width * factor)
     scaledh = int(height * factor)
     return cv2.resize(img, (scaledw, scaledh), interpolation=cv2.INTER_CUBIC), scaledw, scaledh
 
 
-def print_stats(img, w, h, start, count):
-    fps = count/(time.time() - start)
+def print_stats(img, w, h, start):
+    fps = 1/(time.time() - start)
     stats = "FPS: {0}\nResolution: {1}x{2}".format(fps, w, h)
     font = cv2.FONT_HERSHEY_PLAIN
     for i, line in enumerate(stats.split('\n')):
@@ -21,32 +22,103 @@ def print_stats(img, w, h, start, count):
     return img
 
 
+def background_difference(foreground, scale):
+    background = cv2.imread('background.jpg', 0)
+    background, w, h = scale_image(background, scale)
+
+    grey = cv2.cvtColor(foreground, cv2.COLOR_RGB2GRAY)
+
+    difference = cv2.absdiff(grey, background)
+    thresh = cv2.threshold(difference, 25, 255, cv2.THRESH_BINARY)[1]
+
+    kernel = np.ones((2, 2), np.uint8)
+    erode = cv2.erode(thresh, kernel, iterations=1)
+    kernel = np.ones((3, 3), np.uint8)
+    closed = cv2.morphologyEx(erode, cv2.MORPH_CLOSE, kernel)
+
+    return closed
+
+
+def background_MOG(foreground):
+    global substractor
+    if not substractor:
+        substractor = cv2.bgsegm.createBackgroundSubtractorMOG(200, 5, 0.7, 0)
+
+    fgmask = substractor.apply(foreground)
+    return fgmask
+
+
+def background_MOG2(foreground):
+    global substractor
+    if not substractor:
+        substractor = cv2.createBackgroundSubtractorMOG2(500, 50, False)
+
+    fgmask = substractor.apply(foreground)
+    return fgmask
+
+
+def background_KNN(foreground):
+    global substractor
+    if not substractor:
+        substractor = cv2.createBackgroundSubtractorKNN(500, 400, False)
+
+    fgmask = substractor.apply(foreground)
+    return fgmask
+
+
+def sub_background(foreground, scale, method):
+    if method == "resta":
+        return background_difference(foreground, scale)
+    elif method == "MOG":
+        return background_MOG(foreground)
+    elif method == "MOG2":
+        return background_MOG2(foreground)
+    else:
+        return background_KNN(foreground)
+
+
+def draw_contours(img, thresh):
+    contoursim = thresh.copy()
+    im, contours, hierarchy = cv2.findContours(contoursim, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+    for c in contours:
+        if cv2.contourArea(c) < 100:
+            continue
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+
 def main():
     video = sys.argv[1]
     scale = float(sys.argv[2])
+    if len(sys.argv) > 3:
+        method = sys.argv[3]
+    else:
+        method = "resta"
     cap = cv2.VideoCapture(video)
 
     if not cap.isOpened():
         print sys.argv[1] + " couldn't be opened."
 
-    start = time.time()
-    fpscount = 0
-
     while cap.isOpened():
-        # rqead frame
+        start = time.time()
+
+        # read frame
         ret, frame = cap.read()
 
         if ret:
             # scale image
-            frame, width, height = scale_frame(frame, scale)
+            frame, width, height = scale_image(frame, scale)
 
             # operate with frame
+            processed = sub_background(frame, scale, method)
 
+            draw_contours(frame, processed)
             # add stats
-            fpscount += 1
-            frame = print_stats(frame, width, height, start, fpscount)
+            processed = print_stats(processed, width, height, start)
 
-            cv2.imshow('frame', frame)
+            cv2.imshow('processed', processed)
+            cv2.imshow('original', frame)
 
         # end video if q is pressed or no frame was read
         if (cv2.waitKey(30) & 0xFF == ord('q')) or (not ret):
