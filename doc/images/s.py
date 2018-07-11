0859f940b460d13063b4130e6beeac1e22d4b726
Antonio Saavedra <antonio.s814@gmail.com>
2018-07-11 13:18:58 +0200

    Seccion camshift

--- doc/images/s.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-
-b = cv2.imread('clustered.png')
-b = cv2.resize(b, (1000, 750), interpolation=cv2.INTER_CUBIC)
-
-a = cv2.imread('clustering.jpg')
-
-cv2.imwrite('hola.png', np.hstack((a,b)))
